6ce68f0af3709762dd1a6091212086f5f0d0f34c
MeFahmi <yusrianfahmi15@gmail.com>
2025-04-28 23:36:18 +0800

    up code

---
+++ post-test/post-test-6/2409106079-MuhammadIlmaYusrianFahmi-PT-6.cpp
@@ -0,0 +1,461 @@
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+
+using namespace std;
+
+const int MAX_SIRKUIT = 30;
+
+struct Pengelola {
+    string namaPengelola;
+    string kontak;
+};
+///
+struct Sirkuit {
+    string nama;
+    string lokasi;
+    string statusReservasi;
+    Pengelola pengelola;
+};
+
+Sirkuit sirkuit[MAX_SIRKUIT] = {
+    {"Mandalika", "Pulau Lombok", "Tersedia", {"PT Mandalika Racing", "0874-2197-4638"}},
+    {"Internasional Sentul", "Bogor", "Tersedia", {"Sentul Management", "0812-3958-7264"}},
+    {"Jaya Ancol", "Jakarta", "Tersedia", {"Ancol Circuit Ltd", "0899-7321-4186"}},
+    {"e-Prix", "Jakarta", "Tersedia", {"e-Prix Inter", "0831-5064-2987"}},
+    {"Bukit Peusar", "Tasikmalaya", "Tersedia", {"Peusar Motorsports", "0852-7643-1059"}},
+    {"Gokart Subang", "Subang", "Tersedia", {"Subang Racing Club", "0898-2150-7386"}},
+    {"Pancing Circuit", "Medan", "Tersedia", {"Medan Racing Team", "0821-6394-5728"}},
+    {"Sirkuit Skyland", "Palembang", "Tersedia", {"Skyland Motorsport", "0860-4827-3195"}},
+    {"Sirkuit Balipat", "Banjarmasin", "Tersedia", {"Balipat Racing Club", "0815-9730-6241"}},
+    {"Sirkuit Manado", "Manado", "Tersedia", {"Manado Speed Track", "0878-3542-6901"}}
+};
+int panjang = 10;
+
+struct User {
+    string username;
+    string password;
+};
+
+const int MAX_USERS = 100;
+User users[MAX_USERS];
+int jumlahPengguna = 0;
+int pilihan, index;
+
+//id pass admin
+string adminUsername = "Fahmi";
+string adminPassword = "079";
+
+//buat akun
+void registerUser() {
+    if (jumlahPengguna >= MAX_USERS) {
+        cout << "Batas pengguna tercapai!\n";
+    } else {
+        cout << "\n=== Register ===\n";
+        cout << "Masukkan Username: ";
+        cin >> users[jumlahPengguna].username;
+        cout << "Masukkan Password: ";
+        cin >> users[jumlahPengguna].password;
+        cout << "Registrasi Berhasil!\n";
+        jumlahPengguna++;
+    }
+    system("pause");
+}
+
+//kode login
+bool loginUser(bool &isAdmin) {
+    int login = 3;
+    string username, password;
+    //cin.ignore();
+    while (login > 0) {
+        system("cls");
+        cout << "=============================\n";
+        cout << "            LOGIN           \n";
+        cout << "=============================\n";
+        cout << "Masukkan Username: ";
+        getline(cin, username);
+        cout << "Masukkan Password: ";
+        getline(cin, password);
+
+        if (username == adminUsername && password == adminPassword) {
+            isAdmin = true;
+            return true;
+        }
+
+        for (int i = 0; i < jumlahPengguna; i++) {
+            if (users[i].username == username && users[i].password == password) {
+                return true;
+            }
+        }
+
+        login--;
+        cout << "\nGagal! Percobaan tersisa: " << login << "\n";
+        system("pause");
+    }
+    return false;
+}
+
+//rekursif
+void tampilkanSirkuitRekursif(Sirkuit sirkuit[], int index, int jumlahSirkuit) {
+    if (index >= jumlahSirkuit) return;
+    cout << "|  " << setw(1) << index + 1 << "  | "
+         << setw(22) << left << sirkuit[index].nama << " | "
+         << setw(14) << left << sirkuit[index].lokasi << " | "
+         << setw(22) << left << sirkuit[index].pengelola.namaPengelola << " | "
+         << setw(25) << left << sirkuit[index].pengelola.kontak << " | "
+         << setw(16) << left << sirkuit[index].statusReservasi << " |\n";
+    tampilkanSirkuitRekursif(sirkuit, index + 1, jumlahSirkuit);
+}
+
+//menampilkan list sirkuit yang ada
+void tampilkanSirkuit() {
+    system("cls");
+    cout << "\n+-----+------------------------+----------------+------------------------+---------------------------+------------------+\n";
+    cout << "| No  |      Nama Sirkuit      |     Lokasi     |      Pengelola         |          Kontak           | Status Reservasi |\n";
+    cout << "+-----+------------------------+----------------+------------------------+---------------------------+------------------+\n";
+    tampilkanSirkuitRekursif(sirkuit, 0, panjang);
+    cout << "+-----+------------------------+----------------+------------------------+---------------------------+------------------+\n";
+    system("pause");
+}
+
+//mengubah status reservasi sirkuit
+void ubahStatusReservasi() {
+    int statusInput;
+    system("cls");
+    cout << "\n+-----+------------------------+------------------+\n";
+    cout << "| No  |      Nama Sirkuit      | Status Reservasi |\n";
+    cout << "+-----+------------------------+------------------+\n";
+    for (int i = 0; i < panjang; i++) {
+        cout << "|  " << setw(1) << i+1 << "  | "
+             << setw(22) << left << sirkuit[i].nama << " | "
+             << setw(16) << left << sirkuit[i].statusReservasi << " |\n";
+    }
+    cout << "+-----+------------------------+------------------+\n";
+    cout << "\nPilih nomor sirkuit: ";
+    cin >> index;
+    if (index > 0 && index <= panjang) {
+        cout << "\n[1] Tersedia\n";
+        cout << "[2] Digunakan\n";
+        cout << "Pilih Status Reservasi Baru (1/2): ";
+        cin >> statusInput;
+        cin.ignore();
+
+        if (statusInput == 1) sirkuit[index - 1].statusReservasi = "Tersedia";
+        else if (statusInput == 2) sirkuit[index - 1].statusReservasi = "Digunakan";
+        else cout << "Input tidak valid! Harap masukkan 1 atau 2.\n";
+
+        cout << "Status reservasi berhasil diubah menjadi: " << sirkuit[index - 1].statusReservasi << endl;
+    } else {
+        cout << "Nomor tidak valid!\n";
+    }
+    system("pause");
+}
+
+// overloading
+void tambahSirkuit(string nama, string lokasi, string namaPengelola, string kontak) {
+    if (panjang >= MAX_SIRKUIT) {
+        cout << "Batas jumlah sirkuit tercapai!\n";
+    } else {
+        sirkuit[panjang].nama = nama;
+        sirkuit[panjang].lokasi = lokasi;
+        sirkuit[panjang].pengelola.namaPengelola = namaPengelola;
+        sirkuit[panjang].pengelola.kontak = kontak;
+        sirkuit[panjang].statusReservasi = "Tersedia";
+        panjang++;
+        cout << "Sirkuit berhasil ditambahkan!\n";
+    }
+    system("pause");
+}
+
+//menghapus list sirkuit
+void hapusSirkuit() {
+    system("cls");
+    cout << "\n+-----+------------------------+----------------+\n";
+    cout << "| No  |      Nama Sirkuit      |     Lokasi     |\n";
+    cout << "+-----+------------------------+----------------+\n";
+    for (int i = 0; i < panjang; i++) {
+        cout << "|  " << setw(1) << i+1 << "  | "
+             << setw(22) << left << sirkuit[i].nama << " | "
+             << setw(14) << left << sirkuit[i].lokasi << " |\n";
+    }
+    cout << "+-----+------------------------+----------------+\n";
+    cout << "\nPilih nomor sirkuit yang ingin dihapus: ";
+    cin >> index;
+
+    if (index > 0 && index <= panjang) {
+        for (int i = index - 1; i < panjang - 1; i++) {
+            sirkuit[i] = sirkuit[i + 1];
+        }
+        panjang--;
+        cout << "Sirkuit berhasil dihapus!\n";
+    } else {
+        cout << "Nomor tidak valid!\n";
+    }
+    system("pause");
+}
+
+//address-of
+void ubahNamaSirkuit(Sirkuit& s) {
+    cout << "Nama sirkuit saat ini: " << s.nama << endl;
+    cout << "Masukkan nama baru: ";
+    getline(cin, s.nama);
+    cout << "Nama sirkuit berhasil diubah menjadi: " << s.nama << endl;
+}
+
+//dereference
+void tampilkanPengelola(const Pengelola* p) {
+    cout << "Nama Pengelola   : " << p->namaPengelola << endl;
+    cout << "Kontak Pengelola : " << p->kontak << endl;
+}
+
+//bubble huruf ascend
+void bubbleSortNama(Sirkuit sirkuit[], int panjang) {
+    for (int i = 0; i < panjang - 1; i++) {
+        for (int j = 0; j < panjang - i - 1; j++) {
+            if (sirkuit[j].nama > sirkuit[j + 1].nama) {
+                Sirkuit temp = sirkuit[j];
+                sirkuit[j] = sirkuit[j + 1];
+                sirkuit[j + 1] = temp;
+            }
+        }
+    }
+    cout << "Data berhasil diurutkan berdasarkan Nama Sirkuit.\n";
+    system("pause");
+}
+
+//insert huruf ascend
+void insertionSortLokasi(Sirkuit sirkuit[], int panjang) {
+    for (int i = 1; i < panjang; i++) {
+        Sirkuit key = sirkuit[i];
+        int j = i - 1;
+        while (j >= 0 && sirkuit[j].lokasi > key.lokasi) {
+            sirkuit[j + 1] = sirkuit[j];
+            j--;
+        }
+        sirkuit[j + 1] = key;
+    }
+    cout << "Data berhasil diurutkan berdasarkan Lokasi Sirkuit.\n";
+    system("pause");
+}
+
+//selection angka descend
+void selectionSortKontak(Sirkuit sirkuit[], int panjang) {
+    for (int i = 0; i < panjang - 1; i++) {
+        int maxIndex = i;
+        for (int j = i + 1; j < panjang; j++) {
+            if (sirkuit[j].pengelola.kontak > sirkuit[maxIndex].pengelola.kontak) {
+                maxIndex = j;
+            }
+        }
+        if (maxIndex != i) {
+            Sirkuit temp = sirkuit[i];
+            sirkuit[i] = sirkuit[maxIndex];
+            sirkuit[maxIndex] = temp;
+        }
+
+    }
+    cout << "Data berhasil diurutkan berdasarkan Kontak Pengelola.\n";
+    system("pause");
+}
+
+void menuSorting() {
+    int pilihSort;
+    do {
+        system("cls");
+        cout << "=========================================\n";
+        cout << "               MENU SORTING              \n";
+        cout << "=========================================\n";
+        cout << "1. Urutkan Nama Sirkuit (Ascending)\n";
+        cout << "2. Urutkan Lokasi Sirkuit (Ascending)\n";
+        cout << "3. Urutkan Kontak Pengelola (Descending)\n";
+        cout << "4. Kembali\n";
+        cout << "Pilih menu: ";
+        cin >> pilihSort;
+        cin.ignore();
+
+        switch (pilihSort) {
+            case 1:
+                bubbleSortNama(sirkuit, panjang);
+                break;
+            case 2:
+                insertionSortLokasi(sirkuit, panjang);
+                break;
+            case 3:
+                selectionSortKontak(sirkuit, panjang);
+                break;
+            case 4:
+                cout << "Kembali ke menu sebelumnya...\n";
+                system("pause");
+                break;
+            default:
+                cout << "Pilihan tidak valid!\n";
+                system("pause");
+        }
+    } while (pilihSort != 4);
+}
+
+//menu admin
+void menuAdmin() {
+    bool loggedIn = true;
+    while (loggedIn) {
+        system("cls");
+        cout << "=============================\n";
+        cout << "         MENU ADMIN          \n";
+        cout << "         AUTO RACE           \n";
+        cout << "=============================\n";
+        cout << "1. List Sirkuit\n";
+        cout << "2. Ubah Status Reservasi\n";
+        cout << "3. Tambah Sirkuit\n";
+        cout << "4. Hapus Sirkuit\n";
+        cout << "5. Ubah Nama Sirkuit (Pointer)\n";
+        cout << "6. Tampilkan Pengelola (Pointer)\n";
+        cout << "7. Sorting\n";
+        cout << "8. Logout\n";
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+        cin.ignore();
+
+        if (pilihan == 1) tampilkanSirkuit();
+        else if (pilihan == 2) ubahStatusReservasi();
+        else if (pilihan == 3) {
+            string nama, lokasi, pengelola, kontak;
+            cout << "Masukkan Nama Sirkuit: ";
+            cin.ignore();
+            getline(cin, nama);
+            cout << "Masukkan Lokasi: ";
+            getline(cin, lokasi);
+            cout << "Masukkan Nama Pengelola: ";
+            getline(cin, pengelola);
+            cout << "Masukkan Kontak Pengelola: ";
+            getline(cin, kontak);
+            tambahSirkuit(nama, lokasi, pengelola, kontak);
+        }
+        else if (pilihan == 4) hapusSirkuit();
+        else if (pilihan == 5) {
+            system("cls");
+            cout << "Pilih sirkuit yang ingin diubah namanya:\n";
+            for (int i = 0; i < panjang; i++) {
+                cout << i + 1 << ". " << sirkuit[i].nama << endl;
+            }
+            cout << "Pilih nomor: ";
+            cin >> index;
+            cin.ignore();
+        
+            if (index > 0 && index <= panjang) {
+                ubahNamaSirkuit(sirkuit[index - 1]);
+            } else {
+                cout << "Nomor tidak valid!\n";
+            }
+            system("pause");
+        }
+        
+        else if (pilihan == 6) {
+            system("cls");
+            cout << "Pilih sirkuit untuk lihat info pengelola:\n";
+            for (int i = 0; i < panjang; i++) {
+                cout << i + 1 << ". " << sirkuit[i].nama << endl;
+            }
+            cout << "Pilih nomor: ";
+            cin >> index;
+            cin.ignore();
+        
+            if (index > 0 && index <= panjang) {
+                tampilkanPengelola(&sirkuit[index - 1].pengelola);
+            } else {
+                cout << "Nomor tidak valid!\n";
+            }
+            system("pause");
+        }
+        else if (pilihan == 7) menuSorting();
+        else if (pilihan == 8) loggedIn = false;
+    }
+}
+
+//menu user
+void menuUser() {
+    bool loggedIn = true;
+    while (loggedIn) {
+        system("cls");
+        cout << "=============================\n";
+        cout << "         MENU USER           \n";
+        cout << "         AUTO RACE           \n";
+        cout << "=============================\n";
+        cout << "1. List Sirkuit\n";
+        cout << "2. Ubah Status Reservasi\n";
+        cout << "3. Tampilkan Pengelola\n";
+        cout << "4. Sorting\n";
+        cout << "5. Logout\n";
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+        cin.ignore();
+
+        if (pilihan == 1) tampilkanSirkuit();
+        else if (pilihan == 2) ubahStatusReservasi();
+        else if (pilihan == 3) {
+            system("cls");
+            cout << "Pilih sirkuit untuk lihat info pengelola:\n";
+            for (int i = 0; i < panjang; i++) {
+                cout << i + 1 << ". " << sirkuit[i].nama << endl;
+            }
+            cout << "Pilih nomor: ";
+            cin >> index;
+            cin.ignore();
+        
+            if (index > 0 && index <= panjang) {
+                tampilkanPengelola(&sirkuit[index - 1].pengelola);
+            } else {
+                cout << "Nomor tidak valid!\n";
+            }
+            system("pause");
+        }
+        else if (pilihan == 4) menuSorting();
+        else if (pilihan == 5) loggedIn = false;
+    }
+}
+
+//program utama
+int main() {
+    system("cls");
+    bool running = true;
+
+    while (running) {
+        system("cls");
+        cout << "=============================\n";
+        cout << "            LOGIN           \n";
+        cout << "=============================\n";
+        cout << "1. Register\n";
+        cout << "2. Login\n";
+        cout << "3. Keluar\n";
+        cout << "Pilih: ";
+        cin >> pilihan;
+        cin.ignore();
+
+        switch (pilihan) {
+            case 1:
+                registerUser();
+                break;
+
+            case 2: {
+                bool isAdmin = false;
+                if (loginUser(isAdmin)) {
+                    if (isAdmin) menuAdmin();
+                    else menuUser();
+                } else {
+                    cout << "Gagal Login setelah 3 kali Percobaan\n";
+                    system("pause");
+                }
+                break;
+            }
+
+            case 3:
+                running = false;
+                break;
+
+            default:
+                cout << "Pilihan tidak valid!\n";
+                system("pause");
+        }
+    }
+
+    return 0;
+}
